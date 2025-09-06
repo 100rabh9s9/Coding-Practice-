fd8386fb3b6c2e1e430175c9afe85722e92266dd
SOURABH S <100rabh9s9@gmail.com>
2025-09-06 22:01:48 +0530

    Comments on charcount.py

--- charcount.py
+++ charcount.py
@@ -3,11 +3,12 @@ s = input("Enter a string: ").strip()
 encoded = ""
 count = 1
 
-for i in range(1, len(s) + 1):
-    if i < len(s) and s[i] == s[i - 1]:
+for i in range(1, len(s) + 1):  #Traverse through the string
+    if i < len(s) and s[i] == s[i - 1]:  # If current char is same as previous, increase count
         count += 1
-    else:
+    else:   #Otherwise, append previous char and its count to result
         encoded += s[i - 1] + str(count)
         count = 1
 
 print("Encoded string:", encoded)
+
