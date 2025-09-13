7ddb1d52ab2ec4324d246cdc9d794852e44e3232
SOURABH S <100rabh9s9@gmail.com>
2025-09-13 21:49:58 +0530

    Prime Number

--- Prime.py
+++ Prime.py
@@ -1,16 +1,14 @@
 import math #to Perform the Math operation sqrt()
-def is_prime(n):
+def is_prime(n): #Function Definition
     if n <= 1:
         return False  # 0 and 1 are not prime
     for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
             return False  # Found a divisor, not prime
-    return True  # No divisors found, it's prime
+    return True  # If No divisors found, it's prime
 
 num = int(input("Enter a number: "))
- 
 if is_prime(num): #Function Call 
     print(f"{num} is a prime number.")
 else:
     print(f"{num} is not a prime number.")
-
