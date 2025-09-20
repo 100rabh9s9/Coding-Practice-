dbef9ef346cd7943c8f8f18e8c22385c4e4f6f4e
SOURABH S <100rabh9s9@gmail.com>
2025-09-20 22:05:17 +0530

    Pascal's Triangle

--- Pattren1.py
+++ Pattren1.py
@@ -1,8 +1,9 @@
-n = 5
+#Pascals Triangle 
+n = 5 // Fixed Size 
 for i in range(n):
     print(" " * (n - i), end="")
     val = 1
     for j in range(i + 1):
         print(val, end=" ")
-        val = val * (i - j) // (j + 1)
-    print()
+        val = val * (i - j) // (j + 1) //Floor Division 
+    print() //For Proper Spacing 
