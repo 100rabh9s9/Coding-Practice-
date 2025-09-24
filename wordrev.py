1b2c4acef7be2413afaeb719c79b5c6e135f15d7
SOURABH S <100rabh9s9@gmail.com>
2025-09-24 23:05:25 +0530

    Update wordrev.py

--- wordrev.py
+++ wordrev.py
@@ -1,7 +1,9 @@
- def reverse_words(s):
+#Basic Python Program from Leetcode/Hackerank 
+#improvised for self learning 
+def reverse_words(s):
     words = s.split()
     reversed_words = words[::-1]
     return ' '.join(reversed_words)
 
-input_str = "Hello world"
+input_str = "Hello world" #Custom String 
 print(reverse_words(input_str))
