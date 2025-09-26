da3d6c74aa0963dfd74caf5f341fd970ed1a1a44
SOURABH S <100rabh9s9@gmail.com>
2025-09-26 22:53:47 +0530

    OOPs

--- oop.py
+++ oop.py
@@ -1,10 +1,12 @@
-class Student:
-    def __init__(self, name, marks):
-        self.name = name
+
+class Student: #New Class , "the blueprint"
+    def __init__(self, name, marks): #A constructor method that gets called automatically when you create an object.
+        self.name = name  #Assigns the passed value
         self.marks = marks
 
     def display(self):
         print(f"Name: {self.name}, Marks: {self.marks}")
 
-s1 = Student("Alice", 85)
-s1.display()
+s1 = Student("Alice", 85)  #Object Creation
+s1.display() #calling Method
+
