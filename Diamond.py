3879c1dde745c3c4990777e83129d537555ffd24
SOURABH S <100rabh9s9@gmail.com>
2025-09-29 22:29:55 +0530

    Diamond Pattern

--- Diamond.py
+++ Diamond.py
@@ -1,18 +1,18 @@
-def print_diamond(n):
-    
+def pattern_diamond(n):
+    # Upper part of the diamond (including middle row)
     for i in range(n):
         # Print leading spaces
-        print(" " * (n - i - 1), end="")
+        print(" " * (n - i - 1), end=" ")
         # Print stars
         print("*" * (2 * i + 1))
 
     # Lower part of the diamond
     for i in range(n - 2, -1, -1):
         # Print leading spaces
-        print(" " * (n - i - 1), end="")
+        print(" " * (n - i - 1), end=" ")
         # Print stars
         print("*" * (2 * i + 1))
-
-# Test the function
+    #Take input from User to Specify the pattenr number 
 n = int(input("Enter the number of rows for the diamond pattern: "))
-print_diamond(n)
+pattern_diamond(n)
+
